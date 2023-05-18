039f71ddfc7c7f7396816566d41ecd6c3de200d1
Carlos Rendon <3cmrh3@gmail.com>
2023-05-18 14:26:14 -0400

    Attempts to add a DNS

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "random_pet" "rg_name" {
-    prefix = var.resource_group_name_prefix
+    prefix = var.cmrh_resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-    location = var.resource_group_location
+    location = var.cmrh_resource_group_location
     name = random_pet.rg_name.id
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "resource_group_location" {
+variable "cmrh_resource_group_location" {
     default = "eastus"
     description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
+variable "cmrh_resource_group_name_prefix" {
     default = "rg"
     description = "Prefix of the resource group that's combined with a random ID so name is unique in your Azure Subscription"
 }
