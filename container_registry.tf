b05c12c18fdacf9da7cdbbdbbacc3a49f708e9f0
Carlos Rendon <3cmrh3@gmail.com>
2023-05-23 15:18:33 -0400

    Makes data available for AKS usage

--- container_registry.tf
+++ container_registry.tf
@@ -12,3 +12,7 @@ resource "azurerm_role_assignment" "aks_to_acr_role" {
   skip_service_principal_aad_check = true
 }
 
+data "azurerm_container_registry" "az_cr" {
+  name = "cmrhContainerRegistry"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
