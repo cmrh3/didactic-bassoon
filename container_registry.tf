d72bc877be02782a6e35c20b99f845147d4d9375
Carlos Rendon <3cmrh3@gmail.com>
2023-05-23 15:27:07 -0400

    Uses variable explicitly

--- container_registry.tf
+++ container_registry.tf
@@ -6,7 +6,7 @@ resource "azurerm_container_registry" "az_cr" {
 }
 
 resource "azurerm_role_assignment" "aks_to_acr_role" {
-  principal_id                     = azurerm_kubernetes_cluster.aks_default.object_id
+  principal_id                     = var.ARM_CLIENT_ID
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.az_cr.id
   skip_service_principal_aad_check = true
