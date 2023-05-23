15fdc0262ad206594c669a40688dc978bd5fcdbc
Carlos Rendon <3cmrh3@gmail.com>
2023-05-23 15:13:12 -0400

    Attempts to connect CR to AKS

--- container_registry.tf
+++ container_registry.tf
@@ -5,8 +5,8 @@ resource "azurerm_container_registry" "az_cr" {
   sku                 = "Premium"
 }
 
-resource "azurerm_role_assignment" "az_ra_cr_aks" {
-  principal_id                     = azurerm_kubernetes_cluster.aks_default.kubelet_identity[0].object_id
+resource "azurerm_role_assignment" "aks_to_acr_role" {
+  principal_id                     = azurerm_kubernetes_cluster.aks_default.object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.az_cr.id
   skip_service_principal_aad_check = true
